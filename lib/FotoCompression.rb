6ec08819d285dbbfe8960e583b19a3f5c474c81e
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-09-26 21:59:56 +0300

    First Commit.

---
+++ lib/FotoCompression.rb
@@ -0,0 +1,6 @@
+require "FotoCompression/version"
+
+module FotoCompression
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/FotoCompression/version.rb
@@ -0,0 +1,3 @@
+module FotoCompression
+  VERSION = "0.1.0"
+end
